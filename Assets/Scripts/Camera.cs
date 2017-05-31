ef78a9f7da5c4c8236a378d2177e0eb5ac4fab77
Edgard Damiani <edamiani@gmail.com>
2017-05-31 18:29:47 -0300

    Added playre prototype and camera; added .gitignore.

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+	public float		distance = 5;
+	public Transform	objectToFollow;
+	public float		rotationInDegrees = 30;
+
+	private Vector3		mPosition;
+
+	void Start ()
+	{
+		mPosition = objectToFollow.position;
+		mPosition.z = -distance;
+
+		transform.position = mPosition;
+
+		transform.RotateAround(objectToFollow.position, objectToFollow.rotation * Vector3.right, rotationInDegrees);
+		transform.LookAt(objectToFollow);
+	}
+	
+	void Update ()
+	{
+		transform.LookAt(objectToFollow);
+
+		Vector3 direction = objectToFollow.position - transform.position;
+
+		if(direction.magnitude > distance)
+		{
+			transform.position = Vector3.Lerp(transform.position, direction.normalized * Time.deltaTime, 0.5f);
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public float gravity	= 9.8f;
+	public float speed		= 1;
+
+	private CharacterController mCharacterController;
+	private Vector3				mMovement;
+
+	void Start()
+	{
+		mCharacterController = GetComponent<CharacterController>();
+	}
+	
+	void Update()
+	{
+		mMovement.x = Input.GetAxisRaw("Horizontal") * speed;
+		mMovement.y = -gravity * speed;
+		mMovement.z = speed;
+		//mCharacterController.Move(mMovement * Time.deltaTime);
+	}
+}
